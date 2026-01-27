71e9c23f9ae307f7cd0617749a727b8844698168
Hoang <ntphoang205@gmail.com>
2026-01-28 05:59:31 +0700

    Hoàn thành Buoi1_Bai4_ReactJS

--- ReactJS/Learn_ReactJS/src/App.jsx
+++ ReactJS/Learn_ReactJS/src/App.jsx
@@ -2,20 +2,32 @@ import "./App.css";
 import ProductCard from "./Bai1/ProductCard";
 import Button from "./Bai2/Button";
 import Alert from "./Bai3/Alert";
+import LoginForm from "./Bai4/LoginForm";
 import "./Bai1/ProductCard.css";
 import "./Bai2/Button.css";
 import "./Bai3/Alert.css";
+import "./Bai4/LoginForm.css";
 import { useState } from "react";
 
+
 function App() {
-  const [state, setState] = useState(null);
+  // --------------- Bài 3 (Buổi 1) ---------------
+  // const [state, setState] = useState(null);
+  // return (
+  //   <>
+  //     <button onClick={() => setState("success")}>Success</button>
+  //     <button onClick={() => setState("warning")}>Warning</button>
+  //     <button onClick={() => setState("error")}>Error</button>
+
+  //     {state && <Alert type={state} />}
+  //   </>
+  // );
+
+  // --------------- Bài 4 (Buổi 1) ---------------
+
   return (
     <>
-      <button onClick={() => setState("success")}>Success</button>
-      <button onClick={() => setState("warning")}>Warning</button>
-      <button onClick={() => setState("error")}>Error</button>
-
-      {state && <Alert type={state} />}
+      <LoginForm></LoginForm>
     </>
   );
 }
