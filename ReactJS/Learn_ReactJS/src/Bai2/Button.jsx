890a0d8a2fdb1e2c4c41379d19cb6f5ad33acfb3
Hoang <ntphoang205@gmail.com>
2026-01-28 21:13:38 +0700

    restart

--- ReactJS/Learn_ReactJS/src/Bai2/Button.jsx
+++
@@ -1,8 +0,0 @@
-function Button(props) {
-  return (
-    <div>
-      <button className={`btn btn-${props.type}`}>{props.children}</button>
-    </div>
-  );
-}
-export default Button;
--- ReactJS/Learn_ReactJS/src/Bai3/Alert.jsx
+++
@@ -1,9 +0,0 @@
-function Alert( props ) {
-  const message = {
-    success: "Thao tác thành công!",
-    warning: "Cảnh báo! Hãy kiểm tra lại!",
-    error: "Có lỗi xảy ra!",
-  };
-  return <div className={`alert alert-${props.type}`}>{message[props.type]}</div>;
-}
-export default Alert;
