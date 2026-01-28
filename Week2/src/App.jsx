6aaaa439045cf08c7d71ec816a74ae968b1f3e26
Hoang <ntphoang205@gmail.com>
2026-01-28 21:58:41 +0700

    Hoàn thành Buoi2_Bai2_ReactJS

--- Week2/src/App.jsx
+++ Week2/src/App.jsx
@@ -1,22 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import  './Bai1/StudentInfo.css'
-import Header from './Bai1/Header'
-import Footer from './Bai1/Footer'
-import StudentInfo from './Bai1/StudentInfo'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./Bai1/StudentInfo.css";
+import Header from "./Bai1/Header";
+import Footer from "./Bai1/Footer";
+import StudentInfo from "./Bai1/StudentInfo";
 
 function App() {
-  return(
+  // ---------------- Buoi2_Bai1_ReactJS ----------------
+  // return(
+  //   <>
+  //     <Header/>
+  //     <StudentInfo hoten="Nguyễn Trần Phi Hoàng"
+  //     mssv="23711521"
+  //     lop="DHKTPM19B"></StudentInfo>
+  //     <Footer></Footer>
+  //   </>
+  // );
+
+  // ---------------- Buoi2_Bai2_ReactJS ----------------
+  const [count, setCount] = useState(0);
+  return (
     <>
-      <Header/>
-      <StudentInfo hoten="Nguyễn Trần Phi Hoàng" 
-      mssv="23711521" 
-      lop="DHKTPM19B"></StudentInfo>
-      <Footer></Footer>
+      <div>
+        <p>Count: {count}</p>
+      </div>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => Math.max(0, count - 1))}>
+        -
+      </button>
+      <button onClick={() => setCount((count) => 0)}>Restart</button>
     </>
   );
 }
 
-export default App
+export default App;
