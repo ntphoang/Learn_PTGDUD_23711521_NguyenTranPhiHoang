517237a9d34a5e5cb2dae73f036776e4e0cc69b6
Hoang <ntphoang205@gmail.com>
2026-01-30 11:21:57 +0700

    Hoàn thành Buoi2_Bai4_ReactJS

--- Week2/src/App.jsx
+++ Week2/src/App.jsx
@@ -3,9 +3,11 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "./Bai1/StudentInfo.css";
+import "./Bai4/StatusBadge.css";
 import Header from "./Bai1/Header";
 import Footer from "./Bai1/Footer";
 import StudentInfo from "./Bai1/StudentInfo";
+import StatusBadge from "./Bai4/StatusBadge";
 
 function App() {
   // ---------------- Buoi2_Bai1_ReactJS ----------------
@@ -34,28 +36,39 @@ function App() {
   //   </>
   // );
 
-  // ---------------- Buoi3_Bai2_ReactJS ----------------
-  const [ten, setTen] = useState("");
-  const [email, setEmail] = useState("");
+  // ---------------- Buoi2_Bai3_ReactJS ----------------
+  // const [ten, setTen] = useState("");
+  // const [email, setEmail] = useState("");
+  // return (
+  //   <>
+  //     <form action="">
+  //       <input
+  //         type="text"
+  //         placeholder="Tên"
+  //         onChange={(e) => setTen(e.target.value)}
+  //         value={ten}
+  //       />
+  //       <input
+  //         type="text"
+  //         placeholder="Email"
+  //         onChange={(e) => setEmail(e.target.value)}
+  //         value={email}
+  //       />
+  //     </form>
+  //     <h3>THÔNG TIN CỦA BẠN</h3>
+  //     <p>Tên: {ten}</p>
+  //     <p>Email: {email}</p>
+  //   </>
+  // );
+
+  // ---------------- Buoi2_Bai4_ReactJS ----------------
+  const [state, setState] = useState(null);
   return (
     <>
-      <form action="">
-        <input
-          type="text"
-          placeholder="Tên"
-          onChange={(e) => setTen(e.target.value)}
-          value={ten}
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </form>
-      <h3>THÔNG TIN CỦA BẠN</h3>
-      <p>Tên: {ten}</p>
-      <p>Email: {email}</p>
+      <StatusBadge state={state}></StatusBadge>
+      <button onClick={() => setState("online")}>Online</button>
+      <button onClick={() => setState("offline")}>Offline</button>
+      <button onClick={() => setState("busy")}>Busy</button>
     </>
   );
 }
